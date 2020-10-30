f94083669c14e6d6b54ea6b924f780b52f247c20
Jimmy Saenz <jsaenzr@outlook.com>
2020-10-30 13:52:25 -0600

    Agregar documentacion para mayor entendimiento del paso a paso a realizar

--- app/src/main/java/edu/dami/guiameapp/fragments/PointProfileFragment.java
+++ app/src/main/java/edu/dami/guiameapp/fragments/PointProfileFragment.java
@@ -15,6 +15,7 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import android.util.Log;
@@ -50,6 +51,14 @@ public class PointProfileFragment extends Fragment {
 
     private static final String ARG_POINT = "point";
     private static final String TAG = PointProfileFragment.class.getName();
+    /*
+     * PASO 1. Declarar las propiedades a utilizar
+     * - LOC_PERMISSION_REQUEST_CODE: Codigo para identificar evento (onRequestPermissionsResult)
+     *   de obtener ubicacion a traves de Play services
+     * - LOCATION_PERMISSION: Corresponde al permiso que estamos usando en el Manifest
+     * - mLastLocation: Objeto que representa ultima coordenada obtenida a traves
+     *   de Play Services: mLocationClient.getLastLocation()
+    */
     private static final int LOC_PERMISSION_REQUEST_CODE = 300;
     //TODO: si se usa FINE en lugar de COARSE en el manifest, cambiar aqui tambien.
     private static final String LOCATION_PERMISSION = Manifest.permission.ACCESS_COARSE_LOCATION;
@@ -102,10 +111,17 @@ public class PointProfileFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_point_profile, container, false);
     }
 
+    /*
+    PASO 5. Este evento se dispara cuando el usuario ha terminado de interactuar con
+    el dialogo del sistema para solicitar permisos.
+    * */
     @Override
     public void onRequestPermissionsResult(int requestCode,
                                            @NonNull String[] permissions,
                                            @NonNull int[] grantResults) {
+        /*Si es el codigo que definimos en launchLocationPermissionSystemDialog, avancemos y tratemos
+        * de obtener la ubicacion */
+        Log.d(TAG, "onRequestPermissionsResult fired");
         if(requestCode == LOC_PERMISSION_REQUEST_CODE) {
             onUserInteractedWithLocationPermissionSystemDialog(permissions, grantResults);
         }
@@ -125,6 +141,9 @@ public class PointProfileFragment extends Fragment {
         lyRoot = view.findViewById(R.id.ly_root);
         ivPhoto = view.findViewById(R.id.iv_photo);
 
+        /*
+        PASO 2. Configurar referencia de boton y su evento
+        */
         Button btnOpenMap = view.findViewById(R.id.btn_open_map);
         btnOpenMap.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -133,6 +152,9 @@ public class PointProfileFragment extends Fragment {
             }
         });
 
+        /*
+        PASO 3. Inicializar cliente de ubicacion provisto por Play Services
+        */
         setupLocationClient(getContext());
     }
 
@@ -158,14 +180,24 @@ public class PointProfileFragment extends Fragment {
         tryProcessLocation();
     }
 
+    /*
+    PASO 4. Verificar si tenemos permiso para obtener ubicacion del dispositivo
+    En caso que no, solicitarla
+    */
     private void tryProcessLocation() {
+        /*
+        PASO 4.1. La primera vez que cargamos la app lo mas seguro es que no tengamos permisos,
+        asi que el flujo irá hacia el else.
+        */
         if(hasLocationPermission(getContext())) {
             getLocation(getActivity());
         } else {
             requestLocationPermission(getActivity(), getContext());
         }
     }
-
+    /*
+    PASO 4.1. Determina si tenemos permiso de ubicacion
+    */
     private boolean hasLocationPermission(Context context) {
         int permissionState = ContextCompat.checkSelfPermission(
                 context,
@@ -174,18 +206,29 @@ public class PointProfileFragment extends Fragment {
         return permissionState == PackageManager.PERMISSION_GRANTED;
     }
 
+    /*
+    PASO 4.2 Solicitar permiso de Ubicación.
+    */
     private void requestLocationPermission(@Nullable final Activity activity, @Nullable Context context) {
         if(activity == null || context == null) {
             Log.e(TAG, "Por alguna razon, el activity o  el context es null");
             //TODO: presentar algun mensaje en pantalla indicando el error
             return;
         }
+        /*
+        PASO 4.2.1 El sistema decide si es requerido presentar una UI
+        para que le expliquemos al usuario porque estamos solicitando el permiso.
+        Si tenemos que hacerlo, presentamos un Dialogo explicando.
+        */
         if(shouldRequestLocationPermission(activity)) {
             new MaterialAlertDialogBuilder(context)
                     .setTitle(R.string.app_name)
                     .setMessage(R.string.location_request)
                     .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
                         @Override
+                        /*
+                        PASO 4.2.1-A Si el usuario acepta, lanzamos el dialogo del sistema para aceptar el permiso
+                        */
                         public void onClick(DialogInterface dialog, int which) {
                             launchLocationPermissionSystemDialog(activity);
                         }
@@ -198,10 +241,24 @@ public class PointProfileFragment extends Fragment {
                     })
             .show();
         } else {
+            final int locPermission = ContextCompat.checkSelfPermission(context, LOCATION_PERMISSION);
+            /*
+            PASO 4.2.1-B Si el sistema determina que no es requerido presentar una UI para explicarle al usuario,
+            probablemente sea por 2 razones:
+            1. Es la 1ra vez que solicitamos este permiso y el sistema no lo determina como peligroso asi que "pasa cholo"
+            2. Nos bateó y bloqueó la app denegando varias veces el permiso.
+            cargamos de una vez el dialogo del sistema para aceptar el permiso.
+            */
             launchLocationPermissionSystemDialog(activity);
         }
     }
 
+    /*
+    * Lanzar dialogo del sistema para aceptar (o rechazar) el permiso de ubicacion
+    * Nota que pasamos como param LOC_PERMISSION_REQUEST_CODE porque al presentar este dialogo
+    * saldremos temporalmente de nuestra app, cuando el usuario vuelva a nuestra app se disparara
+    * el evento onRequestPermissionsResult y alli usaremos LOC_PERMISSION_REQUEST_CODE
+    * */
     private void launchLocationPermissionSystemDialog(Activity activity) {
         requestPermissions(
                 new String[]{LOCATION_PERMISSION},
@@ -209,6 +266,9 @@ public class PointProfileFragment extends Fragment {
         );
     }
 
+    /*
+    Determina si deberia presentarse una UI para explicarle el usuario porque requerimos el permiso de ubicacion
+    * */
     private boolean shouldRequestLocationPermission(Activity activity) {
         return ActivityCompat.shouldShowRequestPermissionRationale(
                 activity,
@@ -216,6 +276,10 @@ public class PointProfileFragment extends Fragment {
         );
     }
 
+    /*
+    PASO 6. Procesamos la respuesta del usuario al interactuar con el dialogo del sistema de permisos.
+    Podria haber aceptado o denegado el permiso.
+    * */
     private void onUserInteractedWithLocationPermissionSystemDialog(@NonNull String[] permissions,
                                                                     @NonNull int[] grantResults) {
         if (grantResults.length <= 0) {
@@ -224,17 +288,26 @@ public class PointProfileFragment extends Fragment {
         }
         if(grantResults[0] != PackageManager.PERMISSION_GRANTED) {
             showMessageOnSnack("Lo siento, no podemos cargar el punto en el mapa porque no has aceptado el permiso :(");
+            return;
         }
 
         getLocation(getActivity());
     }
 
+    /*
+    * PASO 7. Por fin! podemos empezar a solicitar la ubicacion/coordenadas directamente
+    * */
     @SuppressWarnings("MissingPermission")
     private void getLocation(@Nullable Activity activity) {
         if(activity == null) {
             Log.e(TAG, "Por alguna razon, el activity es null");
             return;
         }
+        /*
+        A traves del cliente de ubicacion nos suscribimos para obtener la ultima ubicacion
+        obtenida del dispositivo en el sistema.
+        Puede que el procesamiento de la coordenada sea exitoso o fallido.
+        * */
         mLocationClient.getLastLocation().addOnCompleteListener(activity, new OnCompleteListener<Location>() {
             @Override
             public void onComplete(@NonNull Task<Location> task) {
@@ -243,6 +316,9 @@ public class PointProfileFragment extends Fragment {
         });
     }
 
+    /*
+    PASO 8. Procesamos el resultado de la ubicacion obtenida desde el cliente de ubicacion
+    * */
     private void processRetrievedLocationTask(@NonNull Task<Location> locationTask) {
         if(!locationTask.isSuccessful() || locationTask.getResult() == null) {
             Log.e(TAG, "Error al obtener coordenada", locationTask.getException());
@@ -259,6 +335,10 @@ public class PointProfileFragment extends Fragment {
         launchMapDirectionWithPointCoords(mLastLocation, mPoint);
     }
 
+    /*
+    * PASO 9. Lanzamos la app de google maps con una ruta precargada tomando como
+    * partida la ubicacion obtenida y como meta la ubicacion del punto.
+    * */
     private void launchMapDirectionWithPointCoords(@Nullable Location userLocation,
                                                    @Nullable PointModel point) {
         if(userLocation == null || point == null) {
