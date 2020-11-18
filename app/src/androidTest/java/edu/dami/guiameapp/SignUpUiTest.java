b1dcc3cc22b4223cf5f6e9a21e6a15d45eedcc47
Jimmy Saenz <jsaenzr@outlook.com>
2020-11-18 10:13:36 -0600

    Remover dependencia innecesaria en test de signup

--- app/src/androidTest/java/edu/dami/guiameapp/SignUpUiTest.java
+++ app/src/androidTest/java/edu/dami/guiameapp/SignUpUiTest.java
@@ -15,8 +15,6 @@ import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.intent.Intents.intended;
-import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
 import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withParent;
